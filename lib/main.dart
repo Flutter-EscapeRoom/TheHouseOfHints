7a85eb5a7d90bc3b8ed8c9cfb5af7584fd946eaf
sharon254 <sharonknyabuti@gmail.com>
2021-10-28 06:40:11 +0300

    Updated Counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Escape Room',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const MyHomePage(title: 'The House of Hints'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -36,37 +39,161 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  /*void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }*/
+
+  void _exponentCounter() {
+    setState(() {
+      //pow(_counter,2) ;
+      _counter=(_counter * _counter);
+    });
+  }
+  void _resetCounter() {
+    setState(() => _counter = 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      /*appBar: AppBar(
+        title: Text(widget.title,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black
+          ),
+        ),
         centerTitle: true,
+      ),*/
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.pinkAccent, Colors.deepPurpleAccent, ])),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                //height: 300,
+                width: 400,
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: <Widget>[
+                        const SizedBox(height: 50),
+                        const Text(
+                          'Welcome to ',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        const SizedBox(height: 15),
+                        const Text(
+                          'House of Hints!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        const SizedBox(height: 150),
+                        Row(
+                          children: [
+                            const Text(
+
+                              'Increment:',
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
+                            ),
+                            Text(
+                              '$_counter',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                          ],
+                        ),
+                        /*Row(
+                          children: [
+                            const Text(
+                              'Decrement:',
+                            ),
+                            Text(
+                              '$_counter',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                          ],
+                        ),*/
+                        const SizedBox(height: 50),
+                        Row(
+                          children: [
+                            const Text(
+                              'Power:',
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
+                            ),
+                            Text(
+                              '$_counter',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 50),
-            const Text(
-              'Hey there, Prisoner🙂\n Wanna escape? Get coding!',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 25,
             ),
-            const SizedBox(height: 150),
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              child: FloatingActionButton(
+                backgroundColor: Colors.black,
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Row(),
+            ),
+            /*Container(
+              child: FloatingActionButton(
+                backgroundColor: Colors.black,
+                onPressed: _decrementCounter,
+                tooltip: 'Decrement',
+                child: const Icon(Icons.remove),
+              ),
+            ),*/
+            Container(
+              child: FloatingActionButton(
+                backgroundColor: Colors.black,
+                onPressed: _exponentCounter,
+                //tooltip: 'Decrement',
+                child: Text('**'),
+              ),
+            ),
+            Expanded(
+              child: Row(),
+            ),
+            Container(
+              child: FloatingActionButton(
+                backgroundColor: Colors.black,
+                onPressed: _resetCounter,
+                //tooltip: 'Decrement',
+                child: Text('reset'),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [Colors.purple, Colors.deepPurpleAccent]
+              )
+          ),
+        ),
+      ),
+    );
+  }
+}
